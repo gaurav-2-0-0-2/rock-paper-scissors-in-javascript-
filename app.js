66601b3cb215ff3bdeee957fd7894d2d0b61dcac
Gaurav Tak <gauravtak996@gmail.com>
2023-01-24 00:42:51 +0530

    server modified

--- app.js
+++ app.js
@@ -2,17 +2,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
-
+const ejs = require('ejs');
 
 const app = express();
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.set('view engine', 'ejs');
 
 app.get("/", (req,res)=>{
-    res.send("hello")
+    res.render("home");
 })
-
 
 
 app.listen(3000, ()=>{
